990cc3d16746980bbf44a4f70520acdce4a0086f
helios <alphatestingdroid@gmail.com>
2020-02-12 12:10:37 +0530

    Initial commit

--- app/src/main/java/com/gifviewer/sample/MainActivity.java
+++ app/src/main/java/com/gifviewer/sample/MainActivity.java
@@ -12,7 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GifViewer gifViewer = findViewById(R.id.gif_view);
-//        gifViewer.setImageResource(R.drawable.hour_glass);
     }
 }
